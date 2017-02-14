1a0c6f78c2803fc6401d1bc7f3a0b5585fc28243
hairybreeches <hairybreeches@localhost>
2017-02-14 14:32:05 +0000

    finding the monthly consumptions

--- src/ovo/core.clj
+++ src/ovo/core.clj
@@ -1,9 +1,32 @@
 (ns ovo.core
   (:gen-class)
   (:require [clojure.math.numeric-tower :refer [expt]]
-           [schema.core :as s]))
+            [schema.core :as s]
+            [medley.core :refer :all]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn- year-month
+  [string-date]
+  (->> string-date
+      (take 6)
+      (apply str)))
+
+(defn- data->map
+  [input]
+  {
+   :date (first (keys input))
+   :consumption (first (vals input))
+   }
+  )
+
+(defn- total-consumption
+  [daily-consumptions]
+  (->> daily-consumptions
+       (map :consumption)
+       (apply +)))
+
+(defn get-month-sum
+  [input]
+  (->> input
+       (map data->map)
+       (group-by (comp year-month :date))
+       (map-vals total-consumption)))
--- test/ovo/core_test.clj
+++ test/ovo/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [ovo.core :refer :all]
             [schema.core :as s]
+            [ovo.data :refer [input]]
             [schema.test]))
 
 (use-fixtures :once schema.test/validate-schemas)
+
+(deftest sums-are-right
+  (is (= {
+          "201512" 60.4
+          "201601" 305.4
+          "201602" 130.0
+          }
+         (get-month-sum input)
+         )))
